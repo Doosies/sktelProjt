fa4b4269c5c67726e339b492fce33a47b8f4ef55
Doosies <song961003@gmail.com>
2021-04-09 00:48:42 +0900

    useTransitions 더 공부

--- front/src/test/Test.js
+++ front/src/test/Test.js
@@ -6,33 +6,36 @@ import _default from 'react-bootstrap/esm/CardColumns';
 
 export default function Test(){
     const [input, setInput] = useState("입력");
-    const id = useRef(5);
+    const id = useRef(4);
     const ref = useRef();
     const [items, setItems] = useState(
       [
-        {id: "1", texts: "1"},
-        {id: "2", texts: "2"},
-        {id: "3", texts: "3"},
-        {id: "4", texts: "4"},
+        {id: 1, texts: "1"},
+        {id: 2, texts: "2"},
+        {id: 3, texts: "3"},
+        {id: 4, texts: "4"},
       ]
     );
 
     const transitions = useTransition(items, {
-      from: { opacity:0, transform: 'translate3d(0,-40px,0)' },
-      enter: { opacity:1, transform: 'translate3d(0,0,0)' },
-      leave: { opacity:0, color: 'red', },
+      duration:10,
+      key: item=>item.id,
+      from: { opacity:0, transform: 'translateY(-40px)'},
+      enter: { opacity:1, transform: 'translateY(0)'},
+      leave: { opacity:0, transform: 'translateY(-40px)'},
       });
     const handleClick = () =>{
+      id.current += 1;
       setItems(items.concat({
         id: id,
         texts: input,
       }));
-      id.current += 1;
       ref.current.focus();
       setInput('');
     }
     const handleDelete = () =>{
-      setItems(items.splice(1));
+      setItems(items.filter(item=> item.id !== id.current));
+      id.current -= 1;
     }
 
     return (
