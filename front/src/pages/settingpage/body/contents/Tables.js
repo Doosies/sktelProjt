b1c9bd047cab40062926b3fab8b89cd328757c4c
Doosies <song961003@gmail.com>
2021-04-09 03:09:19 +0900

    useTransitions -> useSprings로 변경

--- front/src/pages/settingpage/body/contents/Tables.js
+++ front/src/pages/settingpage/body/contents/Tables.js
@@ -1,36 +1,29 @@
-import React, { forwardRef, useEffect, useLayoutEffect } from 'react';
+import React, { forwardRef, useEffect } from 'react';
 import styled, {  } from 'styled-components';
 import { shallowEqual, useDispatch, useSelector } from "react-redux";
 import Row from './Row';
 import { phoneDataFetchAsync } from '../../../../modules/phoneData';
-import { useTransition } from '@react-spring/core';
+import { config, useSprings, useTransition } from '@react-spring/core';
 import { animated } from '@react-spring/web';
 
 const Tables = forwardRef((_,ref) => {
-    // console.log("table");
+    const dispatch = useDispatch();
     const { rows, error,loading} = useSelector( state =>({
         rows: state.phoneData.data.rows,
         loading:state.phoneData.state.loading,
         error:state.phoneData.state.error,
     }), shallowEqual);
-    const dispatch = useDispatch();
-    // console.log(rows.map((data, i) => ({...data, y: i * 40})));
 
-    // const height = 40;
-    // console.log(rows.map((data, i) => ({...data, y: i * 40})));
-    // const transitions = useTransition(
-    //     rows.map((data, i) => ({...data, y: i * 40})),
-    //     row=>{console.log(row); return `row_body_${row.id}`}, 
-    //     {
-    //         from: {position:"absolute", opacity: 0},
-    //         leave: {height:0, opacity: 0},
-    //         enter: ({y}) => ({ y, opacity: 1}),
-    //         update: ({y}) => ({ y }),
-    //     }
-    // );
-    // console.log(transitions);
+    const springs = useSprings(
+        rows.length,
+        rows.map(()=>({
+            opacity:1,transform:"translateX(0px)", 
+            from:{ opacity:0,transform:"translateX(40px)"},
+        }))
+    )
+
     
-    
+
     useEffect(()=>{
         //NOTE - 화면이 로딩될 때 데이터들을 받아와줌.
         dispatch(phoneDataFetchAsync());
@@ -41,17 +34,18 @@ const Tables = forwardRef((_,ref) => {
     return(
         <StyledTable>
             <TableHead>
-                <Row key={`top_row_head`} top />
+                {/* <Row key={`top_row_head`} top /> */}
             </TableHead>
             <TableBody>
                 { loading && null}
                 { !rows && <div>서버로부터 데이터 로딩 실패!</div>}
                 { error && <div>에러 발생 자세한건 로그 참조</div>}
-
-                { rows && rows.map( row =>
+                {/* {rows.map( (row,idx) =>
                     <Row ref={ref} key={`row_body_${row.id}`} rowId={row.id}/>
+                )} */}
+                {springs.map( (style, i) =>
+                        <Row ref={ref} key={`row_body_${rows[i].id}`} rowId={rows[i].id} rowStyle={style}/>
                 )}
-
             </TableBody>
         </StyledTable>
     );
@@ -60,7 +54,7 @@ const Tables = forwardRef((_,ref) => {
 export default React.memo(Tables);
 
 
-const StyledTable = styled(animated.div)`
+const StyledTable = styled.div`
     position:relative;
     width:100%;
     height:100%;
