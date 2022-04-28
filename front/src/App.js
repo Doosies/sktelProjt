020a5732dcd3a55d84d2e8989f44af5eadb2718e
Doosies <song961003@gmail.com>
2022-04-28 23:50:10 +0900

    리액트 경로설정 완료

--- front/src/App.js
+++ front/src/App.js
@@ -2,16 +2,18 @@ import React, {} from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Setting} from './pages/pages';
 
+const AppRoutes = () => {
+    const routes = useRoutes([
+      { path: '/admin', element: <Setting />},
+    ]);
+    return routes;
+  }
 
 function App(){
     return(
         <BrowserRouter>
-            <Switch>
-                <Route path={'/admin'} component={Setting} />
-            </Switch>
+            <AppRoutes />
         </BrowserRouter>
-        
-        
     );
 }
 export default App
