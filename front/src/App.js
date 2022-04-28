4a1a5efe6f658283ae9613a50386b1ea47412105
Doosies <song961003@gmail.com>
2022-04-28 21:11:49 +0900

    라우터 새로추가

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,17 @@
 import React, {} from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Setting} from './pages/pages';
 
 
 function App(){
     return(
-        <Setting />
+        <BrowserRouter>
+            <Switch>
+                <Route path={'/admin'} component={Setting} />
+            </Switch>
+        </BrowserRouter>
+        
+        
     );
 }
 export default App
