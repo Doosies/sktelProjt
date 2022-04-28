f1435c4e7d5e3777f214bb4657a275ca50b87dba
Doosies <song961003@gmail.com>
2022-04-28 13:13:35 +0900

    whitelabel404 해결

--- back/src/main/java/com/ppap/sktel/SktelApplication.java
+++ back/src/main/java/com/ppap/sktel/SktelApplication.java
@@ -2,6 +2,7 @@ package com.ppap.sktel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 // import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 public class SktelApplication {
--- back/src/main/java/com/ppap/sktel/controller/PhoneInfoController.java
+++ back/src/main/java/com/ppap/sktel/controller/PhoneInfoController.java
@@ -2,54 +2,37 @@ package com.ppap.sktel.controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.Map.Entry;
-import java.util.stream.Stream;
-
-import javax.swing.text.html.parser.Entity;
-
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import com.ppap.sktel.phoneInfo.PhoneInfo;
 import com.ppap.sktel.phoneInfo.Service.PhoneInfoService;
 
-import org.apache.ibatis.javassist.expr.NewArray;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.jackson.JsonObjectDeserializer;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 
 
 @RestController
-@CrossOrigin(origins = {"http://localhost:5000", "http://localhost:3000", "http://192.168.206.109:5000"}) 
-public class PhoneInfoController {
+@Controller
+@CrossOrigin(origins = {"http://localhost:5000", "http://localhost:3000", "http://192.168.206.109:5000"})
+public class PhoneInfoController{
  
     @Autowired
     PhoneInfoService phoneInfoService;
     
-    // @RequestMapping(value="/api/phoneinfo", method = RequestMethod.GET)
     @GetMapping(value = "/api/phoneinfo")
     public HashMap<String, Object>  test() {
         HashMap<String, Object> result = new HashMap<String, Object>();
-        // List<PhoneInfo> lists = new ArrayList<PhoneInfo>();
-        // PhoneInfo info1 = new PhoneInfo(1, "model1", "모델1", "10000", "lg", (java.sql.Date) new Date(65456), "1", "1", "1");
-        // lists.add(info1);
-
-        // result.put("lastId", 0);
-        // result.put("rows", lists);
         List<PhoneInfo> lists = phoneInfoService.getAllInfo();
         int lastId =  lists.get(lists.size()-1).getId();
 
@@ -59,8 +42,6 @@ public class PhoneInfoController {
         return result;
     }
 
-    // @RequestMapping(value="/api/phoneinfo/a", method=RequestMethod.PATCH)
-    // @PatchMapping(value = "/api/phoneinfo/a")
     @RequestMapping(value = "api/phoneinfo/a", method = RequestMethod.PATCH, consumes="application/json")
     public  ResponseEntity<String> patchData(@RequestBody Map<String,List<PhoneInfo>> object) {
         System.out.println("------데이터받음------");
